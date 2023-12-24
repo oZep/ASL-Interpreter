9e676672d42a81563cf3de799ab71328c2a6d096
joeyissa <joeyissa59@gmail.com>
2023-12-23 21:10:15 -0500

    hand tracking + drawing hand connections

--- TrackingBase.py
+++ TrackingBase.py
@@ -1,6 +1,37 @@
 import cv2
-import mediapy as mp
+import mediapipe as mp
 import time
 
 cap = cv2.VideoCapture(0)
 
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+prevTime = 0
+currTime = 0
+
+while True:
+    # running webcam
+    success, img = cap.read()
+    imgRBG = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(img)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+
+
+    currTime = time.time()
+    fps = 1/(currTime - prevTime)
+    prevTime = currTime
+
+    cv2.putText(img, str(int(fps)), (10,80), cv2.FONT_HERSHEY_DUPLEX, 3, (0,255,255), 4)
+
+    cv2.imshow("Image", img)
+    if cv2.waitKey(1) & 0xFF == 27:  # Esc to exit the loop
+        break
+
+
+cap.release()
+cv2.destroyAllWindows()
