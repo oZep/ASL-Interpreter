e3eee871993d2ccf796f9c71c616f018252e0544
joeyissa <joeyissa59@gmail.com>
2023-12-23 21:30:44 -0500

    hand tracking + drawing hand connections

--- TrackingBase.py
+++ TrackingBase.py
@@ -19,15 +19,25 @@ while True:
 
     if results.multi_hand_landmarks:
         for handLms in results.multi_hand_landmarks:
-            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)
+            for id, lm in enumerate(handLms.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x*w), int(lm.y*h)
+
+                # drawing circle over hand landmark
+                # cv2.circle(img, (cx,cy), 16, (0,255,255), cv2.FILLED)
+
+            # drawing hand points + lines
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS, mpDraw.DrawingSpec(color=(255, 0, 0)))
 
 
+    # display fps
     currTime = time.time()
     fps = 1/(currTime - prevTime)
     prevTime = currTime
-
     cv2.putText(img, str(int(fps)), (10,80), cv2.FONT_HERSHEY_DUPLEX, 3, (0,255,255), 4)
 
+
+    # display image
     cv2.imshow("Image", img)
     if cv2.waitKey(1) & 0xFF == 27:  # Esc to exit the loop
         break
