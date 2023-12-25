23fb108bf0ea8b388c13cd0d540292b68dedd155
joeyissa <joeyissa59@gmail.com>
2023-12-24 19:33:13 -0500

    we got the first LETTTER!!!!

--- ASLHandDecoder.py
+++ ASLHandDecoder.py
@@ -2,19 +2,19 @@ import cv2
 import mediapipe as mp
 import time
 import math
+import HandTrackingModule as htm
 
 # sort my closeness to next landmark
 # key: 0 value: closest to zero
-
-HAND_SIGNS = {'a': {'POI': [0,1,2,4,6,7,8,10], '0': 12, '1': 2, '2': 4, '3': 2,
-                    '4': 6, '5': -1, '6': 10,'7': 11, '8': 12, '9': -1,
-                    '10': 6, '11': 15, '12': 0, '13': -1, '14': 18, '15': -1,
-                    '16': 17, '18': 15, '19': 15, '20': 14}}
-
+# POI - points of interest
+HAND_SIGNS = {'a': [{'POI': [2, 8, 5, 4, 6, 10, 9, 13, 15, 18, 17, 19]}, {'0': 1, '1': 0, '2': 8, '8': 2, '3': 4, '4': 3, '5': 7, '7': 5, '6': 10, '10': 6, '9': 11, '11': 9, '12': 16, '16': 12, '13': 15, '15': 13, '14': 18, '18': 14, '17': 19, '19': 17, '20': -1, '-1': 20}]}
 
 # what do i quantify as being close
 class ASLDecoder:
     def __init__(self, numHands=2):
+        '''
+        :param numHands: int
+        '''
         self.numHands = numHands
 
     def distanceBetween(self, p1, p2):
@@ -35,25 +35,20 @@ class ASLDecoder:
         '''
         distanceMap = {} # to optamize
 
-        p1 = []
-        p2 = []
-        minDis = 10000 # impossible to get
-        key = -1
+
         # given a matrix with [landmark id, cx, cy]
-        for i in len(landmarks):
+        for i in range(len(landmarks)):
             if str(i) not in distanceMap.keys():
-                p1.append(landmark[i][1])
-                p1.append(landmark[i][2])
-                for j in len(landmarks):
-                    if str(j) not in distanceMap.keys():  # if already located then shortest pair found
-                        p2.append(landmarks[j][1])
-                        p2.append(landmarks[j][2])
-                        dis = self.distanceBetween(p1, p2)
+                minDis = 10000  # impossible to get
+                key = -1
+                for j in range(len(landmarks)):
+                    if i != j and str(j) not in distanceMap.keys():  # if already located then shortest pair found
+                        dis = self.distanceBetween(landmarks[i][1:], landmarks[j][1:])
                         if dis < minDis:
                             minDis = dis
                             key = j
-                distanceMap[str(i)] = j # add the location of it's miminum landmark to the dictionary
-                distanceMap[str(j)] = i
+                distanceMap[str(i)] = key # add the location of it's miminum landmark to the dictionary
+                distanceMap[str(key)] = i
 
         # max complexity: O(q*p) where q = 21 and p = 21, 441 passes
         # will optamized to take half as less passes through skipping previously mapped characters
@@ -61,4 +56,17 @@ class ASLDecoder:
 
 
     def getSign(self, landmarks):
-        self.findNextLandmark(landmarks)
+        closestLandmark = self.findNextLandmark(landmarks)
+        for option, key in enumerate(list(HAND_SIGNS.keys())):
+            check = len(HAND_SIGNS[key][0]['POI'])
+            cashed = 0
+            for POI in HAND_SIGNS[key][0]['POI']: # compare points of interest
+                if closestLandmark.get(str(POI)) == HAND_SIGNS[key][1].get(str(POI)):
+                    cashed += 1
+            if cashed == check:
+                return key
+        return -1
+        #return closestLandmark
+
+
+
--- HandLandmarkPinpointing.py
+++ HandLandmarkPinpointing.py
@@ -2,19 +2,31 @@ import cv2
 import mediapipe as mp
 import time
 import HandTrackingModule as htm
+import ASLHandDecoder as asl
 
 cap = cv2.VideoCapture(0)
 prevTime = 0
 currTime = 0
 
 detector = htm.HandDectector()
+reader = asl.ASLDecoder()
+
+word = ''
 
 while True:
     # running webcam
     success, img = cap.read()
     img = detector.findHands(img)
     lmList = detector.findPosition(img)
+    letter = reader.getSign(lmList)
+    if letter != -1:
+        word += letter
 
+    print(word)
+
+    cv2.putText(img, str(word), (30, 80), cv2.FONT_HERSHEY_DUPLEX, 3, (255, 0, 255), 4)
+
+    # display fps
     # display fps
     currTime = time.time()
     fps = 1 / (currTime - prevTime)
